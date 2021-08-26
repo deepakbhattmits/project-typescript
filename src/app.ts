d1a4fa1812f662aa8c2c4adea10636b5c86627bf
deepakbhattmits <deepakbhattmits@gmail.com>
2021-08-26 08:02:19 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,56 @@
 //
+
+
+const AutoBind = (_: any, _2: string | Symbol, descriptor: PropertyDescriptor) =>
+{
+    const originalMethod = descriptor.value;
+    const adjDescriptor:PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get()
+        {
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        }
+    }
+    return adjDescriptor;
+}
+
 class ProjectInput
 {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+
+    titleElement: HTMLInputElement;
+    descriptionElement: HTMLInputElement;
+    peopleElement: HTMLInputElement;
+
     constructor()
     {
         
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement 
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
         const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild as HTMLFormElement
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleElement = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+    @AutoBind
+    private submitHandler(event:Event)        {
+            event.preventDefault();
+            console.log('values :', this.titleElement.value,this.descriptionElement.value, this.peopleElement.value)
+    }
+    
+    private configure()
+    {
+        this.element.addEventListener('submit', this.submitHandler)
     }
     private attach()
     {
