178a8d29e34f39f72be1aee6944e4ca6619d582a
deepakbhattmits <deepakbhattmits@gmail.com>
2021-08-26 08:02:37 +0530

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,48 @@
 "use strict";
 //
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+const AutoBind = (_, _2, descriptor) => {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+};
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleElement = this.element.querySelector('#title');
+        this.descriptionElement = this.element.querySelector('#description');
+        this.peopleElement = this.element.querySelector('#people');
+        this.configure();
         this.attach();
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        console.log('values :', this.titleElement.value, this.descriptionElement.value, this.peopleElement.value);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 }
+__decorate([
+    AutoBind
+], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput();
